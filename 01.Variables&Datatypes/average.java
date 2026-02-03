a916219bb40ddfebe76494d08723039bf7e15c45
Anish M S <ams631829@gmail.com>
2026-02-03 20:14:23 +0530

    Add files via upload

---
+++ 01.Variables&Datatypes/average.java
@@ -0,0 +1,8 @@
+public class average {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int c=30;
+        System.out.println("avrg of all three numbers is= "+(a+b+c)/2);
+    }
+}
