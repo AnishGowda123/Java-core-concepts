8e4a884be47be22d175a1c40ed5dca5ecbe6ead5
Anish M S <ams631829@gmail.com>
2026-02-03 18:46:48 +0530

    Add files via upload

---
+++ 01.Variables&Datatypes/primitivedata.java
@@ -0,0 +1,21 @@
+public class primitivedata {
+    public static void main(String[] args) {
+        int a= 10;
+        short b = 20;
+        byte c= 7;
+        float e= 10.5f;
+        double f = 20;
+        boolean g= true;
+        System.out.println("int number is="+a);
+        System.out.println("short number is="+b);
+        System.out.println("byte number is="+c);
+        System.out.println("float number is="+e);
+        System.out.println("double number is="+f);
+        System.out.println("boolean value is="+g);
+
+
+    }
+}
+
+
+
