7d37964bd9dcdbad63459220220e62ff906335bc
Anish M S <ams631829@gmail.com>
2026-02-11 22:16:49 +0530

    Add files via upload

---
+++ 04.Looping statements/sumofnumber.java
@@ -0,0 +1,11 @@
+public class sumofnumber {
+    public static void main(String[] args) {
+        int num=1;
+        int sum=0;
+        while (num<=5) {
+            sum=sum+num;
+            num++;
+        }
+        System.out.println("Sum of the number= "+sum);
+    }
+}
