b44cc30c3c605f7609ae406b8e0afa7b0a72f1b4
Anish M S <ams631829@gmail.com>
2026-02-12 22:05:12 +0530

    Add files via upload

---
+++ 05.Methods/product.java
@@ -0,0 +1,9 @@
+public class product {
+    public static void main(String[] args) {
+       System.out.println( "product of the two numbers= "+product(10,30));
+    }
+    public static int product(int a , int b) {
+        int prod=a*b;
+        return prod;
+        }
+}
