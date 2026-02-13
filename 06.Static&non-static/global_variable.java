2e775dfefb3c9344197181cb202374d93b48c36f
Anish M S <ams631829@gmail.com>
2026-02-13 16:38:03 +0530

    Add files via upload

---
+++ 06.Static&non-static/global_variable.java
@@ -0,0 +1,12 @@
+/*Priority in local and global variable*/
+public class basic_static_02 {
+    static int a = 10;
+    public static void main(String[] args) {
+        int a =20;
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println("--------------------");
+        System.out.println(basic_static_02.a);
+        
+    }
+}
---
+++ 06.Static&non-static/without_intilization.java
@@ -0,0 +1,16 @@
+/* In static we can declare variable without intialixation*/ 
+public class basic_static_03 {
+    static int a;
+    static float b;
+    static double c;
+    static boolean d;
+    static short e;
+    public static void main(String[] args) {
+        System.out.println(basic_static_03.a);
+        System.out.println(basic_static_03.b);
+        System.out.println(basic_static_03.c);
+        System.out.println(basic_static_03.d);
+        System.out.println(basic_static_03.e);
+
+    }
+}
