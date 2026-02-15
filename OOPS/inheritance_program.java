f127bbf91b2c72d25ed61f9c6bfe6e1cd155fada
Anish M S <ams631829@gmail.com>
2026-02-15 18:46:17 +0530

    Add files via upload

---
+++ OOPS/inheritance_program.java
@@ -0,0 +1,22 @@
+/*-------------------------parent class------------------------- */
+class parent_class{
+    int x=30;
+    int y=60;
+}
+/*--------------------------child class------------------------*/
+public class inheritance_program extends parent_class{
+    int y=20;
+     public void display(){
+        int x=10;
+        System.out.println(x);
+        System.out.println(y);
+    }
+    public static void main(String[] args) {
+        inheritance_program ob=new inheritance_program();
+        ob.display();
+        System.out.println("--------------------------");
+        System.out.println(ob.x);
+        System.out.println(ob.y);
+
+    }
+}
