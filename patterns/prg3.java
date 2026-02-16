8ab5a7715dcb98d06996290865cdf1192e4a4e05
Anish M S <ams631829@gmail.com>
2026-02-16 23:30:26 +0530

    Add files via upload

---
+++ patterns/prg3.java
@@ -0,0 +1,21 @@
+public class prm3 {
+  
+	public static void main(String[] args) {
+		 int col =5;
+			int row =5;	
+			for (int i = 0; i <row; i++) {
+				char ch='E';
+
+				for (int j = 0; j < col; j++)
+				{
+					 if (i>=j) {
+						System.out.print(ch--);
+				}
+				else {
+						 System.out.print(" ");
+					 }		
+			}
+				System.out.println();
+	}
+	}
+}
