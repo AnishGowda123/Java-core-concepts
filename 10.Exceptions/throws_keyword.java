3d5c75944ecf6ca0fcd8c31d61a55a171e7e774d
Anish M S <ams631829@gmail.com>
2026-02-19 22:32:11 +0530

    Add files via upload

---
+++ 10.Exceptions/throws_keyword.java
@@ -0,0 +1,7 @@
+public class throws_keyword {
+    public static void main(String[] args) throws ArithmeticException {
+        int a =100;
+        int num=a/0;
+        System.out.println(num);
+    }
+}
