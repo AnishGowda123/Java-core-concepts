b0909b8358432dc9df76cd23b0256a25f321d341
Anish M S <ams631829@gmail.com>
2026-02-21 23:26:26 +0530

    Add files via upload

---
+++ 12.String Class/String_method5.java
@@ -0,0 +1,17 @@
+public class String_method5 {
+    public static void main(String[] args) {
+        String s = "Darshan boss";
+        String s1 = "pooja star";
+        String s2 = "krish sir";
+        System.out.println(s.contains("boss"));// contains
+        System.out.println(s1.contains("deep"));
+        System.out.println(s2.contains("ash"));
+        System.out.println("-------------------------");
+        System.out.println(s.concat("-hero"));//concat
+        System.out.println(s1.concat("-heroine"));
+        System.out.println(s2.concat("-director"));
+
+
+        
+    }
+}
