38064f3f80181120e42fd219c55e8d59d62edf2e
Anish M S <ams631829@gmail.com>
2026-02-21 17:00:14 +0530

    Add files via upload

---
+++ 12.String Class/String_constructor.java
@@ -0,0 +1,9 @@
+public class String_constructor {
+    public static void main(String[] args) {
+        String s1 = new String("java");
+        String s2 = new String("python");
+        String s3 = new String("java");
+        System.out.println(s1==s2);
+        System.out.println(s1==s3);
+    }
+}
