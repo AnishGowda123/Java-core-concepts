776dfbb2ddc51e2d8aada54d309b7ee40a22da69
Anish M S <ams631829@gmail.com>
2026-02-25 20:01:20 +0530

    Delete 06.Static&non-static/basic_nonstatic.java

--- 06.Static&non-static/basic_nonstatic.java
+++
@@ -1,9 +0,0 @@
-public class basic_nonstatic {
-    int a=10;
-    int b=20;
-    public static void main(String[] args) {
-        basic_nonstatic ob = new basic_nonstatic();
-        System.out.println(ob.a);
-        System.out.println(ob.b);
-    }
-}
