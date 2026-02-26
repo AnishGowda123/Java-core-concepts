2af3a6c29b27c3a023df7ba76b37a732a8ebe92a
Anish M S <ams631829@gmail.com>
2026-02-26 14:54:02 +0530

    Add files via upload

---
+++ 13.Wrapper class/Autoboxing_3.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Autoboxing_3 {
+    public static void main(String[] args) {
+        ArrayList A = new ArrayList();
+        A.add("anish");
+        A.add("manish");
+        A.add("tanish");
+        System.out.println(A);
+    }
+}
