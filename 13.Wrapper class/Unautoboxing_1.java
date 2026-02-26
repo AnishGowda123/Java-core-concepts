a53389285664e2813bb38a346942b11974c8c8a1
Anish M S <ams631829@gmail.com>
2026-02-26 14:54:36 +0530

    Add files via upload

---
+++ 13.Wrapper class/Unautoboxing_1.java
@@ -0,0 +1,11 @@
+public class Unautoboxing_1 {
+    
+     public static void main(String[] args) {
+        Integer num= new Integer(100);
+        System.out.println(num);
+        test(num);
+    }
+    public static void test(int a){
+        System.out.println(a);
+    }
+}
