c10397902d8dd58b81d7994123995d5e1e38477e
Anish M S <ams631829@gmail.com>
2026-02-26 14:55:05 +0530

    Add files via upload

---
+++ 13.Wrapper class/ParseXXX.java
@@ -0,0 +1,19 @@
+public class ParseXXX {
+    public static void main(String[] args) {
+        String s = "100";
+        String s1 = "19.25";
+
+        int num1= Integer.parseInt(s);
+        int num2= Integer.parseInt(s1);
+        System.out.println("------------------------");
+        float num3= Float.parseFloat(s);
+        float num4= Float.parseFloat(s1);
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+
+    }
+    
+}
