2a28b31bb66d44ac15e442519a814d9e5ec81bcd
Anish M S <ams631829@gmail.com>
2026-03-02 21:59:09 +0530

    Add files via upload

---
+++ 14.Collection Framework/for_eachLoop_3.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class for_eachLoop_3 {  
+     public static void main(String[] args) {
+        ArrayList<String> a = new ArrayList<String>();
+        a.add("java");
+        a.add("python");
+        a.add("c++");
+        a.add("hibernate");
+        a.add("spring");
+        a.add("sql");
+        System.out.println(a);
+        System.out.println("-++++++++++++++++++++++++++++++++");
+    for (String lv : a) {
+            System.out.println(lv);
+        }
+    } 
+}
