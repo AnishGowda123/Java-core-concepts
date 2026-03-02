5c13654c8644731bce566ba0dc60e3e011bab00b
Anish M S <ams631829@gmail.com>
2026-03-02 21:59:29 +0530

    Add files via upload

---
+++ 14.Collection Framework/generics_2.java
@@ -0,0 +1,16 @@
+import java.util.Vector;
+
+public class generics_2 {
+    
+    public static void main(String[] args) {
+        Vector<Integer> a = new Vector<Integer>();
+        a.add(10);
+        a.add(20);
+        a.add(30);
+        a.add(40);
+        a.add(50);
+        a.add(60);
+        System.out.println(a);
+    }
+}  
+
