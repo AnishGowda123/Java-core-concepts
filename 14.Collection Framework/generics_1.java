91b67f11e33391a118563ade6411a5f8a9001fe6
Anish M S <ams631829@gmail.com>
2026-03-03 21:04:36 +0530

    Add files via upload

---
+++ 14.Collection Framework/generics_1.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class generics{
+    public static void main(String[] args) {
+        ArrayList<String> a = new ArrayList<String>();
+        a.add("java");
+        a.add("python");
+        a.add("c++");
+        a.add("hibernate");
+        a.add("spring");
+        a.add("sql");
+        System.out.println(a);
+    }
+
+}
