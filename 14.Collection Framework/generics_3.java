048bcaaa4dbbd79ecf9690d20778f824f78aff85
Anish M S <ams631829@gmail.com>
2026-03-10 22:11:43 +0530

    Add files via upload

---
+++ 14.Collection Framework/generics_3.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class generics_3 {
+    public static void main(String[] args) {
+        ArrayList<Float> a= new ArrayList<Float>();
+        a.add(0.2f);
+        a.add(1.25f);
+        a.add(7.26f);
+        a.add(78.2f);
+        System.out.println(a);
+    }
+}
