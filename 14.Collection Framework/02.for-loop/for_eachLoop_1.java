7444e9c940568211ec7953e99a2b9b9ebf8370da
Anish M S <ams631829@gmail.com>
2026-03-11 21:07:31 +0530

    Add files via upload

---
+++ 14.Collection Framework/02.for-loop/for_eachLoop_1.java
@@ -0,0 +1,9 @@
+public class for_eachLoop_1 {
+    public static void main(String[] args) {
+    String[]a={"java","python","sql","hibernate"};
+    
+    for(String lv:a){
+        System.out.println(lv);
+    }
+  }
+}
