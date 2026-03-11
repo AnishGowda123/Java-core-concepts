651bea5ef1b087e5350f51f0f006322a7bc3e5b4
Anish M S <ams631829@gmail.com>
2026-03-11 21:09:06 +0530

    Add files via upload

---
+++ 14.Collection Framework/02.For-each-loop/for_eachLoop_2.java
@@ -0,0 +1,9 @@
+public class for_eachLoop_2 {
+    public static void main(String[] args) {
+
+        int[]a={10,20,30,40,50,60};
+        for (int lv : a) {
+            System.out.println(a);
+        }
+    }
+}
