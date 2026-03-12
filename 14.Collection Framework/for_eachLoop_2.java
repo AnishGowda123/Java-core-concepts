0a2c387a73281047b0568d8b004cc285d8f0de66
Anish M S <ams631829@gmail.com>
2026-03-12 21:39:44 +0530

    Delete 14.Collection Framework/for_eachLoop_2.java

--- 14.Collection Framework/for_eachLoop_2.java
+++
@@ -1,9 +0,0 @@
-public class for_eachLoop_2 {
-    public static void main(String[] args) {
-
-        int[]a={10,20,30,40,50,60};
-        for (int lv : a) {
-            System.out.println(a);
-        }
-    }
-}
