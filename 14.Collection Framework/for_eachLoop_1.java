d43a712551033ee1bcc80cc162ae5a4adccb3a3f
Anish M S <ams631829@gmail.com>
2026-03-12 21:39:57 +0530

    Delete 14.Collection Framework/for_eachLoop_1.java

--- 14.Collection Framework/for_eachLoop_1.java
+++
@@ -1,9 +0,0 @@
-public class for_eachLoop_1 {
-    public static void main(String[] args) {
-    String[]a={"java","python","sql","hibernate"};
-    
-    for(String lv:a){
-        System.out.println(lv);
-    }
-  }
-}
