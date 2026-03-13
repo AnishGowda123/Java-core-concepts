fc1ff928a69151cadcef310160fe1a8f66573bca
Anish M S <ams631829@gmail.com>
2026-03-13 22:39:44 +0530

    Add files via upload

---
+++ 14.Collection Framework/03.Generics/generics_4.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class generics_4 {
+public static void main(String[] args) {
+    ArrayList<Double> a= new ArrayList<Double>();
+        a.add(0.2452d);
+        a.add(184.25525d);
+        a.add(7102.22216d);
+        a.add(78.2852d);
+        System.out.println(a);
+}
+}
