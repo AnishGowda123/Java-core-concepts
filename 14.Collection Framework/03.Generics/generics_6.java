60efc2196bb433c6f31c60a573821ea7461dba69
Anish M S <ams631829@gmail.com>
2026-03-18 21:23:34 +0530

    Add files via upload

---
+++ 14.Collection Framework/03.Generics/generics_6.java
@@ -0,0 +1,16 @@
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class generics_06 {
+    public static void main(String[] args) {
+        TreeSet<Integer> t= new TreeSet<Integer>();
+        t.add(70);
+        t.add(90);
+        t.add(30);
+        t.add(40);
+        t.add(50);
+        t.add(60);
+        System.out.println(t);
+            
+    }
+}
