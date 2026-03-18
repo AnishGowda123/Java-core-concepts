a211e757dc08103f2de9300f56687e0edb1edbbe
Anish M S <ams631829@gmail.com>
2026-03-18 21:23:05 +0530

    Add files via upload

---
+++ 14.Collection Framework/03.Generics/generics_5.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class generics_5 {
+    public static void main(String[] args) {
+        ArrayList<Long> a = new ArrayList<Long>();
+        a.add(10000l);
+        a.add(2000000l);
+        a.add(3000000l);
+        a.add(4000000l);
+        a.add(5000000l);
+        a.add(60000000l);
+        System.out.println(a);
+       
+    }
+}
