5b449e06323974f16da8c0e44b0a1e58492affa1
Anish M S <ams631829@gmail.com>
2026-03-23 10:26:18 +0530

    Add files via upload

---
+++ 15.Mapping/03.Linkedhashmap/linkedhashmap_3.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+public class linkedhashmap_3 {
+    public static void main(String[] args) {
+        LinkedHashMap m2 = new LinkedHashMap(2);// LinkedHashMap h = new LinkedHashMap(int intialCapacity);
+        m2.put(10, 1.5);
+        m2.put(1.2, "python");
+        System.out.println(m2);
+    } 
+}
