320fbfa1703a5841b8a61160d4e69388391dde46
Anish M S <ams631829@gmail.com>
2026-04-08 23:29:34 +0530

    Add files via upload

---
+++ pattern/prm13.java
@@ -0,0 +1,29 @@
+public class prm13 {
+   
+	
+	public static void main(String[] args) {
+		 int col =5;
+			int row =5;
+			
+			for (int i = 0; i <row; i++) {
+
+				for (int j = 0; j < col; j++)
+				{
+					 if (i>=j) {
+						 
+						System.out.print((i+j+1)%2);
+				}
+				else {
+						 System.out.print(" ");
+					 }
+			}
+				System.out.println();
+				
+
+	}
+	}
+
+
+}
+
+
