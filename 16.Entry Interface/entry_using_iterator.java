9013a3f73eb46af6fdd30b0b652555dd1b37bc8b
Anish M S <ams631829@gmail.com>
2026-04-11 22:54:10 +0530

    Add files via upload

---
+++ 16.Entry Interface/entry_using_iterator.java
@@ -0,0 +1,27 @@
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
+
+public class entry_using_iterator{
+    public static void main(String[] args) {
+         Map<String,Integer> m=new LinkedHashMap<String,Integer>();
+         m.put("java", 10);
+         m.put("python", 25);
+         m.put("html", 65);
+         System.out.println(m);
+         System.out.println("-------------------");
+
+          Set<Entry<String,Integer>> x=m.entrySet();
+
+          Iterator<Entry<String,Integer>> itr=x.iterator();
+          while (itr.hasNext()) {
+
+            Entry<String,Integer> lv= itr.next();
+            System.out.println(lv.getKey()+","+lv.getValue());
+            
+          }
+
+    }
+}
