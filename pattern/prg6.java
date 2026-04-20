0b2d3ecbcbc335d4fd9b08ccca52719c17b553d2
Anish M S <ams631829@gmail.com>
2026-04-20 23:00:38 +0530

    Add files via upload

---
+++ pattern/prg6.java
@@ -0,0 +1,22 @@
+public class prg6 {
+	public static void main(String[] args) {
+		 int col =5;
+			int row =5;
+			int num=5;
+			
+			for (int i = 0; i <row; i++) {
+
+				for (int j = 0; j < col; j++)
+				{
+					 if (i>=j) {
+						System.out.print(num);
+				}
+				else {
+						 System.out.print(" ");
+					 }		
+			}
+				System.out.println();
+				num--;
+	}
+}
+}
