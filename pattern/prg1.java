69b42121184e96cd5933fb9d635f02d9613e7f8b
Anish M S <ams631829@gmail.com>
2026-04-28 23:36:59 +0530

    Add files via upload

---
+++ pattern/prg1.java
@@ -0,0 +1,21 @@
+
+public class prg1 {
+	public static void main(String[] args) {
+		 int col =5;
+			int row =5;
+			for (int i = 0; i <row; i++) {
+				int  num=1;
+
+				for (int j = 0; j < col; j++)
+				{
+					 if (i>=j) {
+						System.out.print(num++);
+				}
+				else {
+						 System.out.print(" ");
+					 }	
+			}
+				System.out.println();
+	}
+	}
+}
